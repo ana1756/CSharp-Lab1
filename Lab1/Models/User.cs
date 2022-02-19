53e9191e8399901009c9eeca217e5b6d7903392d
Anastasia Zolotar <anazolotar28@gmail.com>
2022-02-19 14:20:20 +0200

    setting initial interface components

---
+++ Lab1/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+
+    public class User
+    {
+        private short _date;
+
+        public short Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+    }
+}
---
+++ Lab1/ViewModels/DateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.ViewModels
+{
+    public class DateViewModel
+    {
+
+        private User _user = new User();
+
+        public short Date
+        {
+            get { return _user.Date; }
+            set { _user.Date = value; }
+            
+        }
+
+
+    }
+}
